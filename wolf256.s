89de9f0612c24600b4796a985389d43e1be50e11
Georges <gunstick@syn2cat.lu>
2020-05-18 20:56:04 +0200

    some sound tuning, just going with more buzz tone, that always works

--- wolf256.s
+++ wolf256.s
@@ -6,16 +6,16 @@
   opt  O+ ; all optimizations on 
 
 def_version equ 10
-bootsector equ 10               ;0=code for boot (uses $60000-$60400)
-debug   set  10                  ;0=debug (+ generations in gen:)
+bootsector equ 10   ;0=code for boot (uses $60000-$60400) (never tested)
+debug   set  10     ;0=debug (+ generations in gen:)
 withcls set 10     ; 0=no clear screen
 ownscreen set 10   ; 0=use screen area in bss instead of default OS
 withsound set 0    ; 0=with sound, 10=no sound (needs 48 bytes)
-withcolors set 0    ; 0=with colors, 10=no colors (needs 14 bytes)
+withcolors set 0    ; 0=with colors, 10=no colors (needs 6 bytes)
 vsync set 10   ; 10=fast, no sync ; 0=vsync  (vsync changes from "digit" to "chiptune")
 withmsg set 0 ; 0=with text, 10=no text
 supexec set 10 ; 0=Supexec(xbios); 10=Super(gemdos) (2 bytes less)
-rndinit set 00 ; 0=start with fixed rule ; 10=use whatever start pattern is in d3.b (2 bytes less)
+rndinit set 0 ; 0=start with fixed rule ; 10=use whatever start pattern is in d3.b (6 bytes less)
 gray set 0 ; 0 = play gray codes ; 10 = play binary (gray = 14 bytes more)
 iteration set 1 ; -1 = reverse ; 0 = no rule change ; 1=incremantal  (same size)
 prequel set 5 ; how many rules to play before rule30 (1=play rule30 immediately)
@@ -159,8 +159,13 @@ bootcode:
         ; right part of columns: color 1
         ; text color: color 9
      ifeq withmsg
+      ifeq withcolors
+	; all colors are set in main loop, so does not need init
+        ; move.w #$0777,$ffff824e.w   ; pal 7   (ESC-b-7 text)   #8
+      else
         move.l #$00000777,$ffff8240.w   ; pal 0-1   (background,right columns)    #8
-        move.l #$07770077,$ffff8250.w   ; pal 8-9   (left columns,ESC-b-9 text)   #8
+        move.l #$07770077,$ffff824e.w   ; pal 7-8   (ESC-b-7 text,left columns)   #8
+      endc
      else
          move.w $ffff8250.w,$ffff8242.w   ; pal 8->1   (left=right columns)    #6
 ;        move.w #$0777,$ffff8250.w   ; pal 8   (left columns)   #6
@@ -168,11 +173,11 @@ bootcode:
 
      ifeq rndinit
       ifeq gray
-        moveq #20-iteration*prequel,d3    ; start with rule 30 (30 is the value of the 20th gray code)
+        move.l #(20-iteration*prequel)<<16+$ff,d3    ; start with rule 30 (30 is the value of the 20th gray code)
         ;moveq #0-iteration*prequel,d3    ; start with gray code 0
-        swap d3
+       ; swap d3
       else
-        moveq #30-iteration*prequel,d3
+        moveq.w #30-iteration*prequel,d3
         ;moveq #0-iteration*prequel,d3
       endc
      endc
@@ -218,7 +223,7 @@ cls:
 
 	  swap d3   ; get counter  ; #2    (instead of storing counter on low memory, using high word of d3 is 2 bytes smaller)
 	  ifne iteration
-	    addq.w #iteration,d3   ; #2 iterate through
+	    add.w #iteration,d3   ; #2 iterate through
 	  endc
 	  move.w d3,d4  ; copy counter ; #2
 	  swap d3    ; store counter ; #2
@@ -227,7 +232,7 @@ cls:
 	  eor.w d4,d3
 	else
 	  ifne iteration
-	    addq.w #iteration,d3   ; #2 iterate through
+	    add.w #iteration,d3   ; #2 iterate through
 	  endc	
 	endc   ; gray
   ; not.w $ffff8240.w
@@ -235,14 +240,17 @@ cls:
 ;        move.b #1,32000-80-160(a1)  ; pixel 1 line above so line below is for next generation
         ;move.b #%10001,160*8+80(a1)  ; pixel 1 line above so line below is for next generation
 ;        move.w d3,160*8+16*4+6(a1)  ; just use the rule number as starting seed 
-        eor.l d3,160*8+16*4+6(a1)  ; pixel around middle of screen
+        move.w d3,160*8+16*4+8(a1)  ; pixel around middle of screen
+	ifeq gray
+        move.w d4,160*8+16*5+6(a1)  ; pixel around middle of screen
+	endc
 ;        bset.w #0,160*8+16*1+6(a1)  ; pixel 1 line above so line below is for next generation
 ;        bset.w #0,160*8+6(a1)  ; pixel 1 line above so line below is for next generation
 ;        move.b #1,160*8+80+4(a1)  ; pixel 1 line above so line below is for next generation
 ;        move.b #1,160*8+80+6(a1)  ; pixel 1 line above so line below is for next generation
   ; where we do our business
 loop:
-	
+
       lea 32000(a1),a2
       cmpa.l a2,a0
       bgt.s restart   
@@ -252,21 +260,18 @@ loop:
 ; d0=current value
 ; d1=value to the right
 ; d2=shifted out bits (masked to 3)
-; d5=calculated output
-; d4=%111 (mask for d2)
-; d4=temp register
 ; d3=rule number, i.e. 30 ($1E, %00011110)
+; d4=temp register
+; d5=calculated output
 ; d6=loop counter per column (32)
 ; d7=loop counter for line (10)
 ; a0=beginning of line+6, so first 32bit value is at 0(a0), next at 8(a0) until 72(a0)
 ; a1=pyhsbase
 ; let's first implement an "any rule" algorithm
 columns set 11    ; how many columns to do  (+1 because reasons)
-codeversion set 0 ; 0 = NEWCODE
 ; scan all bits of second last line
         move.l (a0),d0 ; get 32 bits at right edge
 	moveq.l #0,d5
-  ifeq codeversion  ; NEWCODE
         moveq #0,d6   ; fall through 1st loop 
         moveq #(columns-1),d7   ; treat 10 columns
 	lea bof(pc),a4
@@ -276,84 +281,59 @@ _10columns:
 	moveq #30,d6
 _31bitloop:
 	bsr.s dorule
-;        add.l d1,d1     ; #2 shift left
-;        addx.l d0,d0    ; #2 get bit in
-;        addx.b d2,d2    ; #2 accumulate 3 bits
-;        andi.b #%111,d2 ; #4 mask 3 bits
-;        ; code idea by Scarab
-;        btst d2,d3      ; #2 check if matches rule
-;        sne d4          ; #2 set d4 to ff if true
-;        add.b d4,d4     ; #2 move d1 value into X
-;        addx.l d5,d5    ; #2
 	dbf d6,_31bitloop 
 	; do last bit (from new read), then wite and loop
 initload:
         move.l 16(a0),d1 ; get next 32 bits too
 	bsr.s dorule
-;        add.l d1,d1     ; #2 shift left
-;        addx.l d0,d0    ; #2 get bit in
-;        addx.b d2,d2    ; #2 accumulate 3 bits
-;        andi.b #%111,d2 ; #4 mask 3 bits
-;        ; code idea by Scarab
-;        btst d2,d3      ; #2 check if matches rule
-;        sne d4          ; #2 set d4 to ff if true
-;        add.b d4,d4     ; #2 move d1 value into X
-;        addx.l d5,d5    ; #2 add to output
 	move.l d5,(a4)   ; 1st write goes nowhere
 	lea 160(a0),a4   ; next write goes here
-  endc ; NEWCODE
-  ifne codeversion  ; OLDCODE
-;        moveq #-2,d6   ; skip 2 bits at the start
-;        moveq #-1,d6   ; skip 1 bits at the start
-        moveq #0,d6   ; no skip
-        ; we never set the outer bits, is this bad?
-        ; get bit pattern of the 3 pixels above
-        moveq #(columns-1),d7   ; treat 10 columns
-_10columns:
-        move.l 16(a0),d1 ; get next 32 bits too
-        add.w #32,d6  ; treat 32 bits
-_32bitloop:
-        add.l d1,d1   ; shift left
-        addx.l d0,d0  ; get bit in
-        addx.b d2,d2  ; accumulate 3 bits
-        andi.b #%111,d2   ; mask 3 bits
-        ; code idea by Scarab
-        btst d2,d3    ; check if matches rule
-        sne d4        ; set d4 to ff if true
-        add.b d4,d4   ; move d1 value into X
-        addx.l d5,d5  ; add to output
-        dbf d6,_32bitloop
-        move.l d5,160(a0)   ; write line below
-  endc  ; OLDCODE
-        ifeq withsound
-        ;move.b #8,$ffff8800.w ; volume 0
-        ;move.b #0,$ffff8800.w ; tone 0
-          ifeq vsync
-;        move.b d7,$ffff8800.w ; psg register 0-9
-;        move.b d5,$ffff8802.w ; do 'sound'
-          endc
-;        move.b d5,(a6) ; do 'sound'
-          move.b #10,(a6) ; do 'sound'
-;	btst #0,d3
-;	bne.s .nos
-;	  andi.b #$7,d5  ; don't be too loud
-          move.b d5,(a5) ; do 'sound'
-          move.b #3,(a6)
-          move.l d3,(a5)  ;goes from non-pleasing sound to annoying after 256 steps
-;.nos:
-        endc
         ifeq withcolors
-	andi.w #%001100110011,d5
+	  btst.b #0,d3
+	  bne.s nocol
+          ifeq withsound
+            ifeq vsync
+;             move.b d7,$ffff8800.w ; psg register 0-9
+;             move.b d5,$ffff8802.w ; do 'sound'
+            endc
+            move.b #10,(a6) ; do 'sound'
+            move.b d5,(a5) ; do 'digi' sound
+;	    btst #0,d3
+;	    bne.s .nos
+;	    andi.b #$7,d5  ; don't be too loud
+;           move.b (a1,d5.w),(a5) ; do 'sound'
+;           move.b d5,(a5) ; do 'sound'
+;           move.b restart(pc,d3.w),(a5)  
+;.nos:
+          endc
+;	  andi.w #%001100110011,d5
 ;          move.w d3,$ffff8240.w  
 ;          move.w d5,$ffff8242.w
 	  movem.w d3/d5,$ffff8240.w
-          add.w d5,d5
-          move.w d5,$ffff8250.w
+;          add.w d5,d5
+	  not.w d3
+;	  movem.w d3,$ffff8248.w    ; text color
+;         movem.w d5,$ffff8250.w	
+          movem.w d3/d5,$ffff824e.w	
+	  not.w d3
+nocol:
         endc
-
+ 
+        ifeq withsound
+        move.b #11,(a6) ; freqE fine (low byte)
+	move.b d3,d4
+;	lsl.b #4,d4
+;	lsr.b #2,d4    ; avoid low pitched envelope (cut upper 2 bits)
+	bclr #7,d4 ; avoid low pitched envelope 
+	addq.w #8,d4    ; avoid high pitched envelope
+        move.b d4,(a5)  
+;          move.b d3,8(a1)  
+;          move.b d3,8+2(a1)  
+	endc
+ 
         lea 16(a0),a0       ; not 8, as we do 2 columns of 16 pix at once
         dbf d7,_10columns
-        
+         
         lea 160-(columns*16)(a0),a0  ; next line
 ;	lea -(columns*16)(a4),a0   ; this makes interesting forest effects.
         ; vsync
@@ -367,6 +347,7 @@ _32bitloop:
 
         cmpi.b  #$39,$fffffc02.w
         bne     loop
+ 
 ;        bra.s     loop            ; AND HERE IS THE BIG LOOP
 
 dorule:
@@ -384,11 +365,11 @@ exit:
         rts     ; yeah, save 2 bytes, exit with bombs or something... 
       ifeq withmsg
 message:
-        dc.b $1b,"b9"   ; text color: 9
-        dc.b $1b,"E"    ; CLS
+        dc.b $1b,"b7"   ; text color 7 = palette color nr 7
+;        dc.b $1b,"E"    ; CLS  (not needed, .TOS goes to empty text screen (after boot)
         dc.b $1b,"f"    ; cursor off
 message2:
-        dc.b "Gunstick's Wolframmer ",$BD,"ULM 13.5.2020"  ; 38 
+        dc.b "Gunstick's Wolframmer  ULM 18.5.2020"  ; 38 
         dc.b 0
       endc
         ifeq bootsector
@@ -400,15 +381,10 @@ b:
 soundinit:
 	;     13,   12, 11, 10,   9,  8  , 7
 	;     E     freqE   Vc   Vb   Va   Mix
-	dc.b  %1010, 0, $2e, $f, $10, $10, %111100
-;	dc.b  %1010, 0, $2e+72, $f, $10, $10, %111100
+	dc.b  %1110, $0, $0C, $0, $10, $a, %111100
 	;      6,  5,  4,  3,  2,  1,  0
 	;     N   freqC  freqB   freqA
-	dc.b   0,  0,  0,$ee, $e,$ef, $e 
-;	dc.b   0,  0,  0, $ee-5,$e,  $ef, $e 
-        ;     13,   12, 11, 10,   9,  8  , 7
-        ;     E     freqE   Vc   Vb   Va   Mix
-        ;      6,  5,  4,  3,  2,  1,  0
+	dc.b   0,  $f, $f,$7, $80,$e, $a0 
 
 	endc
 ;        endpart
